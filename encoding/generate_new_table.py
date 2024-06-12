7995c4eb467d92d570dee683cce259d08fd12015
Liu <you@example.com>
2024-06-12 22:39:32 +0800

    ascii ignore

--- encoding/generate_new_table.py
+++ encoding/generate_new_table.py
@@ -1,4 +1,5 @@
 import json
+import re
 from collections import Counter, OrderedDict
 
 # Specify the path to your JSON file
@@ -9,25 +10,32 @@ with open(json_file_path, "r", encoding="utf-8") as json_file:
     data = json.load(json_file, object_pairs_hook=OrderedDict)
 
 # Input string and character counting
-test_str = "我能吞下玻璃而不伤身体。"
-char_count = Counter(test_str)
+test_str = "我能吞下玻璃\n而不伤身体。"
+
+# Only keep characters that are not ASCII
+filtered_str = ''.join(char for char in test_str if not char.isascii())
+# Remove non-Chinese characters
+# filtered_str = re.sub(r'[^\u4e00-\u9fff]', '', test_str)
+print(filtered_str)
+char_count = Counter(filtered_str)
+print(char_count)
 sorted_char_count = sorted(char_count.items(), key=lambda x: x[1], reverse=True)
 
 # Replace the values in the JSON data
 char_iter = iter(char for char, _ in sorted_char_count)
 for key in data.keys():
-    if int(key,16) >= 0x829f:
+    # if int(key,16) >= 0x829f:
+    if int(key,16) >= 0x8140:
         try:
             data[key] = next(char_iter)
         except StopIteration:
             break  # No more characters to replace with
 
 # Print the modified data
-for k, v in data.items():
-    print(k, v)
+# for k, v in data.items():
+    # print(k, v)
 
 new_json_file_path = "./new.json"
 
-# Optionally, save the modified JSON back to the file
 with open(new_json_file_path, 'w', encoding="utf-8") as json_file:
     json.dump(data, json_file, ensure_ascii=False, indent=4)
--- encoding/path_table_with_kv.py
+++ encoding/path_table_with_kv.py
@@ -33,12 +33,13 @@ with open(output_file_path, "w", encoding="utf-8") as output_file:
 Patch ELF with the new table.
 """
 
-str = "我能吞下玻璃而不伤身体。"
+str = "我能吞下玻璃\n而不伤身体。"
 # Create a new dictionary with reversed key-value pairs
 reverse_data = {v: k for k, v in data.items()}
 
 # Encode the string
-encoded_s = [reverse_data[char] for char in str if char in reverse_data]
+# if char in ascii, do nothing, else replace with reverse_data[char]
+encoded_s = [reverse_data[char] if char in reverse_data else "0x"+ binascii.hexlify(char.encode('ascii')).decode('ascii') for char in str]
 
 # Print the result
 print(encoded_s)
@@ -50,3 +51,11 @@ print(hex_string)
 # 89e4945c82a189bae0de979e8ea7957382a7906791cc82aa0000
 # Store in Big Endian
 # Test on 0x001B1880
+
+import rzpipe
+
+eboot_file = "../eboot/EBOOT.BIN"
+
+with rzpipe.open(eboot_file, flags=["-w"]) as rz:
+    print(rz.cmd(f"wx {hex_string} @ 0x001B1880"))
+    print(rz.cmd(f"px 10 @ 0x001B1880"))
