a7ded06c5d33798fe95eb37d70c87b4c1579b953
Liu <you@example.com>
2024-06-12 14:40:34 +0800

    Coding Table Finished

--- 2.py
+++ 2.py
@@ -23,20 +23,21 @@ for i in range(0, len(hex_string_mb) - 8, 8):
     # 前两个字节代表编码的起始
     # start_code = int(hex_string_mb[i:i+4], 16)
     start_code = struct.unpack('<H', bytes.fromhex(hex_string_mb[i:i+4]))[0]
-    print(start_code)
+    # print(start_code)
     # 后两个字节代表编码的范围
     next_offset = struct.unpack('<H', bytes.fromhex(hex_string_mb[i+12:i+16]))[0]
     current_offset = struct.unpack('<H', bytes.fromhex(hex_string_mb[i+4:i+8]))[0]
     range_code = next_offset - current_offset
-    print(range_code)
+    # print(range_code)
     # 对于每个编码在范围内的字符
     for j in range(range_code):
         # 计算编码
         code = start_code + j
         # 从1.txt中获取对应的字符
         char = utf16_string_1[current_offset + j]
+        # print(current_offset + j, char, "0x{:04x}".format(code))
         # 将编码和字符添加到字典中
-        byte_char_dict["0x{:04x}".format(code)] = char
+        byte_char_dict[str(hex(code))] = char
 
 # 处理剩余编码
 start_code = struct.unpack('<H', bytes.fromhex(hex_string_mb[-8:-4]))[0]
@@ -44,8 +45,9 @@ range_code = 218
 for j in range(range_code):
     code = start_code + j
     char = utf16_string_1[struct.unpack('<H', bytes.fromhex(hex_string_mb[-4:]))[0] + j]
-    byte_char_dict["0x{:04x}".format(code)] = char
-    print(j)
+    byte_char_dict[str(hex(code))] = char
+    # print(current_offset + j, char, "0x{:04x}".format(code))
+    # print(j)
 
 # 将字典写入JSON文件
 with open('output2.json', 'w', encoding='utf-8') as f:
