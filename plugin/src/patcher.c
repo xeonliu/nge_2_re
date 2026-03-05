9d8575655ec41b6dcef79b81b8fd79c1e7f6b531
liu <62530004+xeonliu@users.noreply.github.com>
2026-03-05 03:03:17 +0000

    fix: replace NEW_ADDR macro with direct value assignment in patchBattleDebugMenu

--- plugin/src/patcher.c
+++ plugin/src/patcher.c
@@ -83,7 +83,7 @@ void patchBattleDebugMenu() {
         // *(u32 *)NEW_ADDR(0x89C97CC) = NEW_ADDR(0x088984C0);
         
         // Battle Debug
-        *(u32 *)NEW_ADDR(0x08b57e04) = NEW_ADDR(0x01);
+        *(u32 *)NEW_ADDR(0x08b57e04) = 0x01;
         
         sceKernelDcacheWritebackAll();
         sceKernelIcacheInvalidateAll();
